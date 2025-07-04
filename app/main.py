2f40b8a3313ba6ac5c9212644edec43a3f180d3c
Kashew Knutt <kashewknutt@gmail.com>
2025-07-04 11:29:10 +0530

    removed .env

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import os
-from dotenv import load_dotenv
-load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), "..", ".env"))
+# from dotenv import load_dotenv
+# load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), "..", ".env"))
 
 
 
